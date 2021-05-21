ae18c8b3252d2cb9041423bb10bdb259878163a5
Wil Taylor <cert@wiltaylor.dev>
2021-05-21 12:52:21 +1000

    coffee shop mock up

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import ( "fmt")
+import ( "fmt"; "flag"; "os"; )
+
+func usage() {
+  fmt.Println("kn command [options]")
+  fmt.Println("")
+  fmt.Println("Commands:")
+  fmt.Println("new - Create a new note.")
+
+}
+
+func newNote() {
+  notePath := os.Getenv("ZKDIR")
+  if notePath == "" {
+    notePath = "Empty"
+  }
+
+  fmt.Printf("%s", notePath)
+}
 
 func main() {
-  fmt.Println("Working")
+  flag.Parse()
+  cmd := flag.Arg(0)
+
+
+  switch(cmd) {
+  case "new":
+    newNote()
+  default:
+   usage()
+  }
 }
