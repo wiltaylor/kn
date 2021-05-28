fe592c75202d135b366f68a1422a98491c695217
Wil Taylor <cert@wiltaylor.dev>
2021-05-28 17:25:08 +1000

    Refectored into multiple files and added link addition

---
+++ data.go
@@ -0,0 +1,367 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type NoteState int
+type NoteType int
+type LinkType int
+
+const (
+	NewState NoteState = iota
+	GreenState
+	DoneState
+	UnknownState
+)
+
+const (
+	ZettleNote NoteType = iota
+	MapNote
+	LiteratureNote
+	FleetingNote
+	UnknownNote
+)
+
+const (
+	LinkUrl LinkType = iota
+	LinkAttachment
+	LinkNote
+	LinkEmpty
+)
+
+type NoteHeader struct {
+	Title    string
+	Id       string
+	Filename string
+	Date     string
+	Type     NoteType
+	State    NoteState
+}
+
+type NoteLink struct {
+	Title string
+	Type  LinkType
+	Path  string
+	Id    int
+}
+
+type NoteData struct {
+	Header       NoteHeader
+	RawText      string
+	FormatedText string
+	Links        []NoteLink
+}
+
+var AllNotes []NoteHeader
+
+func GetHeaderFromFile(id string) (NoteHeader, error) {
+	path := filepath.Join(NoteDirectory, id+".md")
+	result := NoteHeader{Title: "", Id: id, Filename: filepath.Join(NoteDirectory, id+".md"), Date: "", Type: UnknownNote, State: UnknownState}
+
+	file, err := os.Open(path)
+
+	if err != nil {
+		return result, err
+	}
+
+	s := bufio.NewScanner(file)
+
+	//Stripping the top ---
+	s.Scan()
+
+	if s.Text() != "---" {
+		return result, errors.New("File header doesn't start with ---")
+	}
+
+	for s.Scan() {
+		txt := s.Text()
+
+		if txt == "---" {
+			break
+		}
+
+		if strings.HasPrefix(txt, "Title:") {
+			result.Title = strings.Trim(txt[6:], " ")
+			continue
+		}
+
+		if strings.HasPrefix(txt, "Date:") {
+			result.Date = strings.Trim(txt[5:], " ")
+			continue
+		}
+
+		if strings.HasPrefix(txt, "Type:") {
+			typ := strings.Trim(txt[5:], " ")
+
+			if typ == "zettle" {
+				result.Type = ZettleNote
+				continue
+			}
+
+			if typ == "literature" {
+				result.Type = LiteratureNote
+				continue
+			}
+
+			if typ == "fleeting" {
+				result.Type = FleetingNote
+				continue
+			}
+
+			if typ == "map" {
+				result.Type = MapNote
+				continue
+			}
+
+			result.Type = UnknownNote
+			continue
+		}
+
+		if strings.HasPrefix(txt, "Status:") {
+			status := strings.Trim(txt[7:], " ")
+
+			if status == "new" {
+				result.State = NewState
+				continue
+			}
+
+			if status == "done" {
+				result.State = DoneState
+				continue
+			}
+
+			if status == "green" {
+				result.State = GreenState
+				continue
+			}
+
+			result.State = UnknownState
+			continue
+		}
+	}
+
+	return result, nil
+}
+
+func FindNotes(pattern string, noteTypes []NoteType) []NoteHeader {
+	result := make([]NoteHeader, 0)
+
+  pattern = strings.ToLower(pattern)
+
+	for _, note := range AllNotes {
+		m := false
+		for _, t := range noteTypes {
+			if note.Type == t {
+				m = true
+				break
+			}
+		}
+
+		if m == false {
+			continue
+		}
+
+		match, _ := regexp.MatchString(pattern, strings.ToLower(note.Title))
+
+		if match {
+			result = append(result, note)
+		}
+	}
+
+	return result
+}
+
+func RefreshNotes() error {
+	AllNotes = make([]NoteHeader, 0)
+
+	files, err := ioutil.ReadDir(NoteDirectory)
+
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		matched, err := regexp.MatchString(`\.md$`, file.Name())
+		if err != nil {
+			continue
+		}
+
+		if !matched {
+			continue
+		}
+
+		id := strings.ReplaceAll(file.Name(), ".md", "")
+		header, err := GetHeaderFromFile(id)
+
+		if err != nil {
+			continue
+		}
+
+		AllNotes = append(AllNotes, header)
+
+	}
+
+	return nil
+}
+
+func RefreshNote(id string) error {
+  idx := -1
+
+	for i, note := range AllNotes {
+		if note.Id == id {
+			idx = i
+			break
+		}
+	}
+
+	if idx == -1 {
+		return errors.New("No note with that name found!")
+	}
+
+	newNote, err := GetHeaderFromFile(id)
+	AllNotes[idx] = newNote
+
+	return err
+}
+
+func NewNote(title string, noteType NoteType) (NoteData, error) {
+	curTime := time.Now().UTC()
+	atomicId := fmt.Sprintf("%v", curTime.Unix())
+	path := filepath.Join(NoteDirectory, fmt.Sprintf("%v.md", atomicId))
+
+	header := NoteHeader{Title: title, Id: atomicId, Filename: path, Date: curTime.Format(time.RFC822)}
+	result := NoteData{Header: header, RawText: "", FormatedText: "", Links: make([]NoteLink, 0)}
+
+	err := SaveNoteData(result)
+
+  AllNotes = append(AllNotes, header)
+
+	return result, err
+}
+
+func GetNoteData(header NoteHeader) (NoteData, error) {
+	result := NoteData{Header: header, RawText: "", FormatedText: "", Links: make([]NoteLink, 0)}
+
+	byteData, err := ioutil.ReadFile(header.Filename)
+
+	if err != nil {
+		return result, err
+	}
+
+	text := string(byteData)
+
+	index := 0
+	remain := 2
+	currentCount := 0
+
+	for i, c := range text {
+		if c == '-' {
+			currentCount += 1
+		} else {
+			currentCount = 0
+		}
+
+		if currentCount == 3 {
+			currentCount = 0
+			remain -= 1
+		}
+
+		if remain == 0 {
+			index = i + 2 // Also eat new line
+			break
+		}
+	}
+
+	text = text[index:]
+
+	result.RawText = text
+
+	link := regexp.MustCompile(`\[(.+)\]\((.+)\)`)
+	linkMatches := link.FindAllStringSubmatch(text, -1)
+
+	id := 0
+	for i := range linkMatches {
+		typ := LinkUrl
+
+		if strings.HasPrefix(linkMatches[i][2], "zk:") {
+			typ = LinkNote
+		}
+
+		if strings.HasPrefix(linkMatches[i][2], "zka:") {
+			typ = LinkAttachment
+		}
+
+		lnk := NoteLink{Title: linkMatches[i][1], Path: linkMatches[i][2], Type: typ, Id: id}
+
+		result.Links = append(result.Links, lnk)
+		id += 1
+	}
+
+	return result, nil
+}
+
+func SaveNoteData(note NoteData) error {
+	noteTypeText := "zettle"
+	statusText := "new"
+
+	if note.Header.Type == ZettleNote {
+		noteTypeText = "zettle"
+	}
+
+	if note.Header.Type == MapNote {
+		noteTypeText = "map"
+	}
+
+	if note.Header.Type == LiteratureNote {
+		noteTypeText = "literature"
+	}
+
+	if note.Header.Type == FleetingNote {
+		noteTypeText = "fleeting"
+	}
+
+	if note.Header.State == NewState {
+		statusText = "new"
+	}
+
+	if note.Header.State == DoneState {
+		statusText = "done"
+	}
+
+	if note.Header.State == GreenState {
+		statusText = "green"
+	}
+
+	file, err := os.Create(note.Header.Filename)
+
+	if err != nil {
+		return err
+	}
+
+	writer := bufio.NewWriter(file)
+	writer.WriteString("---\n")
+	writer.WriteString(fmt.Sprintf("Title: %s\n", note.Header.Title))
+	writer.WriteString(fmt.Sprintf("Date: %s\n", note.Header.Date))
+	writer.WriteString(fmt.Sprintf("Type: %s\n", noteTypeText))
+	writer.WriteString(fmt.Sprintf("Status: %s\n", statusText))
+	writer.WriteString("---\n")
+	writer.WriteString(note.RawText)
+	writer.Flush()
+	file.Close()
+
+	return nil
+
+}
---
+++ ui.go
@@ -0,0 +1,517 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"fmt"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+const (
+	LinkIconUrl        = ""
+	LinkIconNote       = ""
+	LinkIconAttachment = ""
+)
+
+type ViewMode int
+
+const (
+	ViewModeMain ViewMode = iota
+	ViewModeSearch
+	ViewModeSearchLink
+)
+
+// Main screen
+var app *tview.Application
+var toolbar *tview.TextView
+var textbox *tview.TextView
+var mainLayout *tview.Grid
+
+// Search screen
+var searchLayout *tview.Grid
+var searchField *tview.InputField
+var searchResult *tview.Table
+var typeForm *tview.Form
+var zettleCheck *tview.Checkbox
+
+// Search Checkboxes
+var zettleChk, mapChk, litChk, fleetingChk bool
+
+var CurrentViewMode ViewMode
+var CurrentSearchResults []NoteHeader
+var CurrentNote NoteData
+var CurrentSearchSelection int
+var CurrentLinkIndex int
+var NoteHistory []string
+
+func InitUI() {
+	app = tview.NewApplication()
+
+	CurrentViewMode = ViewModeMain
+
+	// Main view controls
+	toolbar = tview.NewTextView()
+	toolbar.SetText("ESC-Quit|N-New|F-Find|E-Edit|A-AddLink|HJKL-Move|Enter-FollowLink|Backspace-Back")
+	toolbar.SetBackgroundColor(tcell.ColorWhite)
+	toolbar.SetTextColor(tcell.ColorBlack)
+
+	textbox = tview.NewTextView()
+	textbox.SetText("")
+	textbox.SetScrollable(true)
+	textbox.SetTitle("Empty")
+	textbox.SetBorder(true)
+	textbox.SetBorderPadding(0, 0, 1, 1)
+	textbox.SetDynamicColors(true)
+	textbox.SetRegions(true)
+	CurrentLinkIndex = -1
+
+	mainLayout = tview.NewGrid()
+	mainLayout.SetRows(1, 5, 0)
+	mainLayout.AddItem(toolbar, 0, 0, 1, 1, 1, 1, false)
+	mainLayout.AddItem(textbox, 1, 0, 10, 1, 10, 40, true)
+
+	// Search Window controls
+	searchField = tview.NewInputField()
+	searchField.SetText("")
+	searchField.SetChangedFunc(SearchUpdate)
+	searchField.SetLabel("Note Title: ")
+
+	typeForm = tview.NewForm()
+	typeForm.AddCheckbox("Zettle", true, func(checked bool) {
+		zettleChk = checked
+	})
+	zettleChk = true
+
+	typeForm.AddCheckbox("Map", true, func(checked bool) {
+		mapChk = checked
+	})
+	mapChk = true
+
+	typeForm.AddCheckbox("Literature", false, func(checked bool) {
+		litChk = checked
+	})
+	litChk = false
+	typeForm.AddCheckbox("Fleeting", false, func(checked bool) {
+		fleetingChk = checked
+	})
+	fleetingChk = false
+
+	typeForm.SetHorizontal(true)
+	typeForm.SetBorder(true)
+
+	searchResult = tview.NewTable()
+	searchResult.SetSelectable(true, false)
+	searchResult.SetSelectionChangedFunc(func(row int, _ int) {
+		CurrentSearchSelection = row
+	})
+
+	searchLayout = tview.NewGrid()
+	searchLayout.SetRows(1, 1, 0)
+	searchLayout.SetMinSize(0, 0)
+	searchLayout.AddItem(searchField, 0, 0, 1, 1, 1, 1, true)
+	searchLayout.AddItem(typeForm, 2, 0, 1, 1, 1, 40, false)
+	searchLayout.AddItem(searchResult, 3, 0, 10, 1, 10, 40, false)
+
+	app.SetInputCapture(handleInput)
+	app.SetRoot(mainLayout, true)
+	app.SetFocus(textbox)
+
+	NoteHistory = make([]string, 0)
+}
+
+func SwitchView(mode ViewMode) {
+	switch mode {
+	case ViewModeMain:
+		app.SetRoot(mainLayout, true)
+		break
+	case ViewModeSearch:
+		app.SetRoot(searchLayout, true)
+		app.SetFocus(searchField)
+		SearchUpdate(searchField.GetText())
+		break
+	case ViewModeSearchLink:
+		app.SetRoot(searchLayout, true)
+		app.SetFocus(searchField)
+		SearchUpdate(searchField.GetText())
+		break
+	}
+
+	CurrentViewMode = mode
+}
+
+func SearchUpdate(txt string) {
+	typs := make([]NoteType, 0)
+
+	if zettleChk {
+		typs = append(typs, ZettleNote)
+	}
+
+	if mapChk {
+		typs = append(typs, MapNote)
+	}
+
+	if litChk {
+		typs = append(typs, LiteratureNote)
+	}
+
+	if fleetingChk {
+		typs = append(typs, FleetingNote)
+	}
+
+	CurrentSearchResults = FindNotes(txt, typs)
+
+	searchResult.Clear()
+
+	for idx, item := range CurrentSearchResults {
+		searchResult.SetCellSimple(idx, 0, item.Title)
+	}
+
+	if len(CurrentSearchResults)-1 > CurrentSearchSelection {
+		CurrentSearchSelection = 0
+	}
+}
+
+func handleInput(event *tcell.EventKey) *tcell.EventKey {
+
+	if CurrentViewMode == ViewModeMain {
+		if event.Key() == tcell.KeyEsc {
+			ShutdownUI()
+			return nil
+		}
+
+		if event.Key() == tcell.KeyBackspace || event.Key() == tcell.KeyBackspace2 {
+			if len(NoteHistory) <= 1 {
+				return nil
+			}
+
+			id := NoteHistory[len(NoteHistory)-2]
+
+			NoteHistory = NoteHistory[0 : len(NoteHistory)-2]
+
+			head, err := GetHeaderFromFile(id)
+
+			if err != nil {
+				panic(err)
+			}
+
+			d, err := GetNoteData(head)
+
+			if err != nil {
+				panic(err)
+			}
+
+			CurrentNote = d
+			RefreshFileView()
+			return nil
+		}
+
+		if event.Key() == tcell.KeyEnter {
+			if CurrentLinkIndex == -1 {
+				return nil
+			}
+
+			if len(CurrentNote.Links) == 0 {
+
+				return nil
+			}
+
+			lnk := CurrentNote.Links[CurrentLinkIndex]
+
+			if lnk.Type == LinkUrl {
+				cmd := exec.Command("xdg-open", lnk.Path)
+				cmd.Start()
+
+				return nil
+			}
+
+			if lnk.Type == LinkAttachment {
+				if len(lnk.Path) <= 4 {
+					return nil
+				}
+
+				path := filepath.Join(NoteDirectory, ".attachments", lnk.Path[4:])
+
+				cmd := exec.Command("xdg-open", path)
+				cmd.Start()
+				return nil
+			}
+
+			if lnk.Type == LinkNote {
+				id := lnk.Path[3:]
+				h, err := GetHeaderFromFile(id)
+
+				if err != nil {
+
+					return nil
+				}
+
+				n, err := GetNoteData(h)
+
+				if err != nil {
+
+					return nil
+				}
+
+				CurrentNote = n
+				NoteHistory = append(NoteHistory, CurrentNote.Header.Id)
+				RefreshFileView()
+
+			}
+
+			return nil
+		}
+
+		if event.Key() == tcell.KeyTab {
+			if len(CurrentNote.Links) == 0 {
+				return nil
+			}
+
+			CurrentLinkIndex++
+
+			if CurrentLinkIndex >= len(CurrentNote.Links) {
+				CurrentLinkIndex = 0
+			}
+
+			textbox.Highlight(fmt.Sprintf("%v", CurrentNote.Links[CurrentLinkIndex].Id))
+			textbox.ScrollToHighlight()
+
+			return nil
+		}
+
+		if event.Rune() == 'f' {
+			SwitchView(ViewModeSearch)
+			return nil
+		}
+
+		if event.Rune() == 'e' {
+			if CurrentNote.Header.Filename != "" {
+				EditFile(CurrentNote.Header.Filename)
+				RefreshNote(CurrentNote.Header.Id)
+				RefreshFileView()
+			}
+			return nil
+		}
+
+		if event.Rune() == 'a' {
+			SwitchView(ViewModeSearchLink)
+			return nil
+		}
+
+		if event.Rune() == 'n' {
+			note, err := NewNote("New Note", ZettleNote)
+
+			NoteHistory = append(NoteHistory, note.Header.Id)
+
+			if err != nil {
+				panic(err)
+			}
+
+			CurrentNote = note
+			EditFile(CurrentNote.Header.Filename)
+			RefreshNote(CurrentNote.Header.Id)
+			RefreshFileView()
+		}
+	}
+
+	if CurrentViewMode == ViewModeSearch || CurrentViewMode == ViewModeSearchLink {
+		if event.Key() == tcell.KeyEsc {
+			SwitchView(ViewModeMain)
+			return nil
+		}
+
+		if event.Key() == tcell.KeyLeft {
+			idx := 0
+
+			for {
+				if idx >= typeForm.GetFormItemCount() {
+					return nil
+				}
+
+				ctrl := typeForm.GetFormItem(idx)
+				if ctrl.HasFocus() {
+
+					if idx == 0 {
+						app.SetFocus(typeForm.GetFormItem(typeForm.GetFormItemCount() - 1))
+						return nil
+					}
+
+					app.SetFocus(typeForm.GetFormItem(idx - 1))
+					return nil
+				}
+				idx++
+			}
+		}
+
+		if event.Key() == tcell.KeyRight {
+			idx := 0
+
+			for {
+				if idx >= typeForm.GetFormItemCount() {
+					return event
+				}
+
+				ctrl := typeForm.GetFormItem(idx)
+				if ctrl.HasFocus() {
+
+					if idx == typeForm.GetFormItemCount()-1 {
+						app.SetFocus(typeForm.GetFormItem(0))
+						return nil
+					}
+
+					app.SetFocus(typeForm.GetFormItem(idx + 1))
+					return nil
+				}
+
+				idx++
+			}
+		}
+
+		if event.Key() == tcell.KeyEnter {
+			if len(CurrentSearchResults) == 0 {
+				return nil
+			}
+
+			if CurrentViewMode == ViewModeSearchLink {
+				note := CurrentSearchResults[CurrentSearchSelection]
+				CurrentNote.RawText += fmt.Sprintf("\n[%s](zk:%s)\n", note.Title, note.Id)
+        SaveNoteData(CurrentNote)
+			} else {
+
+				n, err := GetNoteData(CurrentSearchResults[CurrentSearchSelection])
+
+				if err != nil {
+					return nil
+				}
+
+				CurrentNote = n
+				NoteHistory = append(NoteHistory, CurrentNote.Header.Id)
+			}
+			RefreshFileView()
+			SwitchView(ViewModeMain)
+		}
+
+		if event.Key() == tcell.KeyTab {
+			if searchField.HasFocus() {
+				app.SetFocus(searchResult)
+				return nil
+			}
+
+			if searchResult.HasFocus() {
+				app.SetFocus(typeForm)
+				return nil
+			}
+
+			if typeForm.HasFocus() {
+				app.SetFocus(searchField)
+				return nil
+			}
+			return nil
+		}
+
+		if event.Key() == tcell.KeyF5 {
+			RefreshNotes()
+			SearchUpdate(searchField.GetText())
+		}
+	}
+
+	return event
+}
+
+func ShutdownUI() {
+	app.Stop()
+	app = nil
+}
+
+func RunUI() {
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func RefreshFileView() {
+	CurrentNote, err := GetNoteData(CurrentNote.Header)
+
+	if err != nil {
+		panic(err)
+	}
+
+	FormatCurrentFile(&CurrentNote)
+
+	textbox.SetText(CurrentNote.FormatedText)
+	textbox.SetTitle(CurrentNote.Header.Title)
+	textbox.Highlight()
+	textbox.ScrollToBeginning()
+	CurrentLinkIndex = -1
+}
+
+func EditFile(filename string) {
+	editor := os.Getenv("EDITOR")
+
+	if editor == "" {
+		editor = "vim"
+	}
+
+	app.Suspend(func() {
+		cmd := exec.Command(editor, filename)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		cmd.Run()
+	})
+
+}
+
+func FormatCurrentFile(file *NoteData) {
+	text := file.RawText
+
+	h6 := regexp.MustCompile(`###### (.+)\n`)
+	h5 := regexp.MustCompile(`##### (.+)\n`)
+	h4 := regexp.MustCompile(`#### (.+)\n`)
+	h3 := regexp.MustCompile(`### (.+)\n`)
+	h2 := regexp.MustCompile(`## (.+)\n`)
+	h1 := regexp.MustCompile(`# (.+)\n`)
+	b1 := regexp.MustCompile(`\n [-|*|+] `)
+	b2 := regexp.MustCompile(`\n   [-|*|+] `)
+	b3 := regexp.MustCompile(`\n     [-|*|+] `)
+	l1 := regexp.MustCompile(`\n ([0-9]{1,5})\. `)
+	l2 := regexp.MustCompile(`\n   ([0-9a-z]{1,5})\. `)
+	l3 := regexp.MustCompile(`\n     ([0-9a-z]{1,5})\. `)
+
+	for _, lnk := range file.Links {
+		ico := LinkIconUrl
+
+		if lnk.Type == LinkNote {
+			ico = LinkIconNote
+		}
+
+		if lnk.Type == LinkAttachment {
+			ico = LinkIconAttachment
+		}
+
+		lnkText := fmt.Sprintf("[%s](%s)", lnk.Title, lnk.Path)
+		newText := fmt.Sprintf("[\"%v\"]%s[blue::u]%s[-:-:-][\"\"]", lnk.Id, ico, lnk.Title)
+		text = strings.Replace(text, lnkText, newText, 1)
+	}
+
+	text = h6.ReplaceAllString(text, "     [green::b] $1[-:-:-]\n")
+	text = h5.ReplaceAllString(text, "    [green::b] $1[-:-:-]\n")
+	text = h4.ReplaceAllString(text, "   [green::b] $1[-:-:-]\n")
+	text = h3.ReplaceAllString(text, "  [green::b] $1[-:-:-]\n")
+	text = h2.ReplaceAllString(text, " [blue::b] $1[-:-:-]\n")
+	text = h1.ReplaceAllString(text, "[red::b] $1[-:-:-]\n")
+
+	text = b1.ReplaceAllString(text, "\n [green]ﱣ[-] $1")
+	text = b2.ReplaceAllString(text, "\n   [green]ﱤ[-] $1")
+	text = b3.ReplaceAllString(text, "\n     [green][-] $1")
+	text = l1.ReplaceAllString(text, "\n [green]$1.[-]")
+	text = l2.ReplaceAllString(text, "\n   [green]$1.[-]")
+	text = l3.ReplaceAllString(text, "\n     [green]$1.[-]")
+
+	file.FormatedText = text
+}
